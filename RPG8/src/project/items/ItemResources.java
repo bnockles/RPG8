92acf60f39376e11ce167efa1d30ecc2660c411e
Calvin <Calvin@Calvin-PC>
2016-02-03 18:36:48 -0500

    Jia Added more on ItemResources and Weapons

--- RPG8/src/project/items/ItemResources.java
+++ RPG8/src/project/items/ItemResources.java
@@ -13,10 +13,27 @@ public class ItemResources {
 	 * Weapons(name, desc, ammo, damage, ammoClip, ammoCapacity, criticalHit, critHitMul, 
 	  		weight, buyable, cost, rof, rofRPS, reloadSpd, armourPiercing, 
 	  		secondaryFire, canSilence, gunNum)
+	  	
+	  	How 'gunNum' works
+	  	
+	  	First Number:
+	  	1. Melee 
+		2. Pistols
+		3. Rifles
+		4. Heavy
+		5. SMG
+		6. Explosives
+		7. Plasma
+		
+		Second Number:
+		Which gun in the category. ex pistol3 is 3
+		
+		So gunNum is 23
+
 	 */
 	Weapons pistol3 = new Weapons("CT-Roaring Thunder", "Pistol", pistolAmmo, 62, 20, 140, .20, 2, 
 			1, false, 0.0, "Auto", 13, .55, true, true, true, 3);
-	Weapons pistol4 = new Weapons("Desert Phoenix", "Pistol", pistolAmmo, 88, 7, 70, .35, 2, 
+	Weapons pistol4 = new Weapons("Desert Phoenix", "Pistol", pistolAmmo, 88, 7, 70, .35, 2,
 			1, true, 1850.00, "Semi", 0, 1.2, true, false, false, 4);
 	
 }
--- RPG8/src/project/items/Weapons.java
+++ RPG8/src/project/items/Weapons.java
@@ -1,13 +1,47 @@
 package project.items;
 
 public class Weapons extends Item{
-
+	Ammo ammo;
+	int damage;
+	int ammoClip;
+	int ammoCapacity;
+	double criticalHit;
+	int critHitMul;
+	int weight;
+	boolean buyable;
+	double cost;
+	String rof;
+	int rofRPS;
+	double reloadSpd;
+	boolean armourPiercing;
+	boolean secondaryFire;
+	boolean canSilence;
+	int gunNum;
+	
 	public Weapons(String name, String desc, Ammo ammo, int damage, int ammoClip, int ammoCapacity,
 			double criticalHit, int critHitMul, int weight, boolean buyable, double cost, String rof, int rofRPS, 
 			double reloadSpd, boolean armourPiercing, boolean secondaryFire, boolean canSilence, int gunNum) {
 		super(name, desc);
-		// TODO Auto-generated constructor stub
+		
+		this.ammo = ammo;
+		this.damage = damage;
+		this.ammoClip = ammoClip;
+		this.ammoCapacity = ammoCapacity;
+		this.criticalHit = criticalHit;
+		this.critHitMul = critHitMul;
+		this.weight = weight;
+		buyable = true;
+		this.cost = cost;
+		this.rof = rof;
+		this.rofRPS = rofRPS;
+		this.reloadSpd = reloadSpd;
+		armourPiercing = false;
+		secondaryFire = false;
+		canSilence = true;
+		this.gunNum = gunNum;
 	}
 	
-	
+	public Weapons getGun(String name, int gunNum){
+		return null;
+	}
 }
