67a7a94c7c652ff8d0a4c9483bca71f17096c07b
Calvin <Calvin@Calvin-PC>
2016-02-03 18:18:42 -0500

    Jia Added Weapons and Constructor

--- RPG8/src/project/items/ItemResources.java
+++ RPG8/src/project/items/ItemResources.java
@@ -1,7 +1,22 @@
 package project.items;
 
 public class ItemResources {
+	//Medkits
 	MedKit smallkit=new MedKit("Small Medkit","It's crap.",50);
+	
+	//Ammo
 	Ammo shotgunAmmo=new Ammo("Shotgun Ammo", "It's shotgun ammo", 1);
+	Ammo pistolAmmo=new Ammo("Pistol Ammo", "It's pistol ammo", 1);
+	
+	//Weapons
+	/**
+	 * Weapons(name, desc, ammo, damage, ammoClip, ammoCapacity, criticalHit, critHitMul, 
+	  		weight, buyable, cost, rof, rofRPS, reloadSpd, armourPiercing, 
+	  		secondaryFire, canSilence, gunNum)
+	 */
+	Weapons pistol3 = new Weapons("CT-Roaring Thunder", "Pistol", pistolAmmo, 62, 20, 140, .20, 2, 
+			1, false, 0.0, "Auto", 13, .55, true, true, true, 3);
+	Weapons pistol4 = new Weapons("Desert Phoenix", "Pistol", pistolAmmo, 88, 7, 70, .35, 2, 
+			1, true, 1850.00, "Semi", 0, 1.2, true, false, false, 4);
 	
 }
--- RPG8/src/project/items/Weapons.java
+++ RPG8/src/project/items/Weapons.java
@@ -2,7 +2,9 @@ package project.items;
 
 public class Weapons extends Item{
 
-	public Weapons(String name, String desc, Ammo ammo, int damage, int ammoClip, int ammoCapacity) {
+	public Weapons(String name, String desc, Ammo ammo, int damage, int ammoClip, int ammoCapacity,
+			double criticalHit, int critHitMul, int weight, boolean buyable, double cost, String rof, int rofRPS, 
+			double reloadSpd, boolean armourPiercing, boolean secondaryFire, boolean canSilence, int gunNum) {
 		super(name, desc);
 		// TODO Auto-generated constructor stub
 	}
