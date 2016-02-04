f96cfe07161eb26844fbb1121c3c39c357868593
Mark Mozgovoy <mmozgovoy7464@bths.edu>
2016-02-03 22:13:56 -0500

    Tooltip - added image x,y coordinates

--- RPG8/src/project/menus/Tooltip.java
+++ RPG8/src/project/menus/Tooltip.java
@@ -9,15 +9,19 @@ public class Tooltip extends VerticalMenu{
 	int height;
 	int x;
 	int y;
+	int imgX;
+	int imgY;
 	String text;
 	BufferedImage image;
 	
-	public Tooltip(int width, int height, int x, int y, String text, BufferedImage image) {
+	public Tooltip(int width, int height, int x, int y, String text, BufferedImage image, int imgX, int imgY) {
 		super(width, height);
 		this.x = x;
 		this.y = y;
 		this.text = text;
 		this.image = image;
+		this.imgX = imgX;
+		this.imgY = imgY;
 	}
 
 	@Override
@@ -25,7 +29,4 @@ public class Tooltip extends VerticalMenu{
 		//We don't put selectors into a tooltip, just a text box.
 		//or we could add a single selector, which is just text, and nothing happens when it is clicked.
 	}
-
-	
-	
 }
