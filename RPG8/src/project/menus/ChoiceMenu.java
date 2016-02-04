8fe78cf3a1bcbf261f0b6282f1bb5910ef9b5179
msulavko7355 <msulavko7355@bths.edu>
2016-02-03 23:47:27 -0500

    Added parts

--- RPG8/src/project/menus/ChoiceMenu.java
+++ RPG8/src/project/menus/ChoiceMenu.java
@@ -5,8 +5,13 @@ import java.util.List;
 
 public class ChoiceMenu extends GridMenu {
 	
+	String description;
+	String dialogue;
 	
-	public ChoiceMenu(int width, int height, int itemsAcross, int itemsDown, BufferedImage nullImage, List<Selectable> page, String font) {
+	public ChoiceMenu(int width, int height, int itemsAcross, int itemsDown, BufferedImage nullImage, 
+	List<Selectable> page, String description, String dialogue) {
 		super(width, height, itemsAcross, itemsDown, nullImage, page); //w and h defined later
+		this.description = description;
+		this.dialogue;
 	}
 }
