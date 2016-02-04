3ba8e3aee7cc88058c106a005dd63e33fd92810e
Mark Mozgovoy <mmozgovoy7464@bths.edu>
2016-02-03 22:36:53 -0500

    Tooltip - added private stuff

--- RPG8/src/project/menus/Tooltip.java
+++ RPG8/src/project/menus/Tooltip.java
@@ -5,14 +5,14 @@ import java.awt.image.BufferedImage;
 
 public class Tooltip extends VerticalMenu{
 	
-	int width;
-	int height;
-	int x;
-	int y;
-	int imgX;
-	int imgY;
-	String text;
-	BufferedImage image;
+	private int width = 50;
+	private int height = 100;
+	private int x;
+	private int y;
+	private int imgX;
+	private int imgY;
+	private String text;
+	private BufferedImage image;
 	
 	public Tooltip(int width, int height, int x, int y, String text, BufferedImage image, int imgX, int imgY) {
 		super(width, height);
@@ -22,6 +22,53 @@ public class Tooltip extends VerticalMenu{
 		this.image = image;
 		this.imgX = imgX;
 		this.imgY = imgY;
+	}
+	
+	public Tooltip(int width, int height, int x, int y, String text) {
+		super(width, height);
+		this.x = x;
+		this.y = y;
+		this.text = text;
+	}
+	
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public int getImgX() {
+		return imgX;
+	}
+
+	public int getImgY() {
+		return imgY;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public BufferedImage getImage() {
+		return image;
 	}
 
 	@Override
