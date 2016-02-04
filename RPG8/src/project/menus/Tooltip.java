601d0c3c84909adb0a50e0b17cad538cab82c51e
Mark Mozgovoy <mmozgovoy7464@bths.edu>
2016-02-03 21:44:02 -0500

    A thing

---
+++ RPG8/src/project/menus/Tooltip.java
@@ -0,0 +1,31 @@
+package project.menus;
+
+import java.awt.Graphics2D;
+
+public class Tooltip extends SimpleSelection{
+
+	public Tooltip(int w, int h) {
+		super(w, h);
+	}
+
+	@Override
+	public void confirm() {
+		
+	}
+
+	@Override
+	public void update() {
+		
+	}
+
+	@Override
+	public void paintSelected(Graphics2D g2) {
+		
+	}
+
+	@Override
+	public void paintUnselected(Graphics2D g2) {
+		
+	}
+	
+}
