f8050ea949ec428fbf5135854065033576eb1384
msulavko7355 <msulavko7355@bths.edu>
2016-02-03 21:47:19 -0500

    Update ChoiceMenu.java
    fixed error

--- RPG8/src/project/menus/ChoiceMenu.java
+++ RPG8/src/project/menus/ChoiceMenu.java
@@ -7,4 +7,5 @@ public class ChoiceMenu extends GridMenu {
 	
 	public ChoiceMenu(int width, int height, int itemsAcross, int itemsDown, BufferedImage nullImage, List<Selectable> page, String font) {
 		super(width, height, itemsAcross, itemsDown, nullImage, page);
+	}
 }
