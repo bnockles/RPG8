a0bc514befee1d6aabfb2968f6dc105a16dc64fb
KVP <KVP@KVP-PC.home>
2016-02-04 00:02:49 -0500

    Things

--- RPG8/src/project/menus/MainMenu.java
+++ RPG8/src/project/menus/MainMenu.java
@@ -1,16 +1,19 @@
 package project.menus;
 
+import java.util.ArrayList;
+
 public class MainMenu extends VerticalMenu{
+	
+	 ArrayList<Selectable> selections = new ArrayList<Selectable>();
 
 	public MainMenu(int width, int height) {
 		super(width, height);
-		// TODO Auto-generated constructor stub
+		//this.selections.add(null);
+		//Will add 4 parts of the selection
 	}
 
 	@Override
 	public void addSelections() {
-		// TODO Auto-generated method stub
-		
+		//The 4 Selections: Start New Game, Load Game, Settings, and Help will be added here
 	}
-
 }
