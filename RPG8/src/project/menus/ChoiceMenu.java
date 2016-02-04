d092c58c4bd23335d84b5db610f59a62d2b1b403
msulavko7355 <msulavko7355@bths.edu>
2016-02-03 21:49:14 -0500

    Added own w & h

--- RPG8/src/project/menus/ChoiceMenu.java
+++ RPG8/src/project/menus/ChoiceMenu.java
@@ -5,7 +5,10 @@ import java.util.List;
 
 public class ChoiceMenu extends GridMenu {
 	
+	int w;
+	int h;
+	
 	public ChoiceMenu(int width, int height, int itemsAcross, int itemsDown, BufferedImage nullImage, List<Selectable> page, String font) {
-		super(width, height, itemsAcross, itemsDown, nullImage, page);
+		super(w, h, itemsAcross, itemsDown, nullImage, page); //w and h defined later
 	}
 }
