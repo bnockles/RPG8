cf7eceb906742fc543287674a2651468edf1b1e6
CChen9935 <CChen9935@bths.edu>
2016-02-08 22:59:43 -0500

    Add more files

--- RPG8/src/project/battles/Projectiles.java
+++ RPG8/src/project/battles/Projectiles.java
@@ -1,9 +1,11 @@
 package project.battles;
 
 public class Projectiles {
-	private int[] stats;
-
-	public Projectiles(int[] stats){
-		this.stats=stats;
+	private int damage;
+	public Projectiles(int damage){
+		this.damage = damage;
+	}
+	public int getDamage() {
+		return damage;
 	}
 }
