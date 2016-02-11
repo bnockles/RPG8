c49abf9e3bb89224da74101af23c3b2b4bbe24db
awong2142 <awong2142@bths.edu>
2016-02-10 21:05:56 -0500

    some demo code

--- RPG8/src/project/overworld/Boundaries.java
+++ RPG8/src/project/overworld/Boundaries.java
@@ -1,5 +1,15 @@
 package project.overworld;
 
-public class Boundaries extends Obstacle {
+import java.awt.Image;
 
+public class Boundaries extends Obstacle {
+	public boolean lethal;
+	public Boundaries(String name, int xPos, int yPos, int sizeX, int sizeY,Image image, boolean lethal) {
+		super(name, xPos, yPos, sizeX, sizeY,image);
+		this.lethal = lethal;
+	}
+	public boolean isLethal() {
+		return lethal;
+	}
+	
 }
--- RPG8/src/project/overworld/Obstacle.java
+++ RPG8/src/project/overworld/Obstacle.java
@@ -1,5 +1,52 @@
 package project.overworld;
 
-public abstract class Obstacle {
+import java.awt.image.BufferedImage;
+import java.awt.Image;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Toolkit;
+
+public class Obstacle {
+	public String name;
+	public int xPos;
+	public int yPos;
+	public int sizeX;
+	public int sizeY;
+	Image image;
+	public Obstacle(String name, int xPos, int yPos, int sizeX, int sizeY,Image image) {
+		this.name = name;
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.sizeX = sizeX;
+		this.sizeY = sizeY;
+		this.image = image;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+
+	public int getxPos() {
+		return xPos;
+	}
+
+	public int getyPos() {
+		return yPos;
+	}
+
+	public int getSizeX() {
+		return sizeX;
+	}
+
+	public int getSizeY() {
+		return sizeY;
+	}
+	public void drawImage(Graphics g){
+		Graphics2D g2 = (Graphics2D) g;
+		g2.drawImage(image,xPos,yPos,null);
+		
+	}
+	
 
 }
