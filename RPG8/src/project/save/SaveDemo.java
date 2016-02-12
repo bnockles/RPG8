d420bb291bb579fee608af176f149cfe27207d76
swat2129 <swat2129@bths.edu>
2016-02-12 14:02:23 -0500

    Added SaveDemo

---
+++ RPG8/src/project/save/SaveDemo.java
@@ -0,0 +1,30 @@
+package project.save;
+
+import java.awt.event.KeyListener;
+
+import project.directors.Game;
+import project.directors.Screen;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+
+public class SaveDemo extends Screen {
+	public static void main(String[] args) {
+		
+	}
+	
+	public SaveDemo(Game game) {
+		super(game);
+	}
+	
+	public void paintScreen(Graphics2D g2) {
+		
+	}
+	
+	public KeyListener getKeyListener() {
+		return this;
+	}
+	
+}
