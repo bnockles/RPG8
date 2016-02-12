59ae04bf8f4eaaa9a86eb7d7cae6857359a73238
Student8 <Student8@10.8.33.84>
2016-02-12 14:02:08 -0500

    learned to make Itemdemo itemscreen

---
+++ RPG8/src/project/items/ItemDemo.java
@@ -0,0 +1,26 @@
+package project.items;
+
+import project.items.ItemDemo;
+import project.items.ItemScreen;
+import project.directors.Game;
+import project.directors.Screen;
+
+public class ItemDemo extends Game {
+
+	public static void main(String[] args) {
+		new ItemDemo();
+	}
+	
+//	public EnemyDemo() {
+//		applySettings();//display the JFrame the way I want it
+//		reset();//starts the game from the beginning
+//		setVisible(true);
+//	}
+	
+	public void reset() {
+		Screen demoScreen = new ItemScreen(this);
+		setScreen(demoScreen);
+		repaint();
+	}
+
+}
