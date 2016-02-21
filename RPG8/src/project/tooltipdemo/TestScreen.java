e1cad520b7e4d66c0276b3aff46c1e66df6f0c19
Mark Mozgovoy <mmozgovoy7464@bths.edu>
2016-02-21 14:34:07 -0500

    Updated with further functionality

--- RPG8/src/project/tooltipdemo/TestScreen.java
+++ RPG8/src/project/tooltipdemo/TestScreen.java
@@ -3,29 +3,47 @@ package project.tooltipdemo;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.ArrayList;
 
 import project.directors.Game;
 import project.directors.Screen;
+import project.mainmenudemo.MainMenuSelectables;
 
 public class TestScreen extends Screen implements KeyListener {
 
+	public static int selection = 0;
+	ArrayList<TooltipOption> options;
+	
 	public TestScreen(Game game) {
 		super(game);
-		// TODO Auto-generated constructor stub
+		new OptionList();
+		options.add(new TooltipOption("Option 1"));
+		options.add(new TooltipOption("Option 2"));
+		options.add(new TooltipOption("Option 3"));
+		options.add(new TooltipOption("Option 4"));
 	}
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 
 	@Override
-	public void keyPressed(KeyEvent arg0) {
+	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+		if(e.getKeyCode() == KeyEvent.VK_DOWN){
+			selection++;
+			if(selection > 3){
+				selection = 0;
+			}
+			update();
+		}
+		if(e.getKeyCode() == KeyEvent.VK_UP){
+			selection--;
+			if(selection < 0){
+				selection = 3;
+			}
+			update();
+		}
 	}
 
 	@Override
