6fa1cb1f761e68698fc38fdf8a62aac5d7b0a92e
Jase <Jase@Jase-Jase>
2016-02-22 20:00:13 -0500

    Projectiles Class

--- RPG8/src/project/battles/Projectiles.java
+++ RPG8/src/project/battles/Projectiles.java
@@ -1,11 +1,43 @@
 package project.battles;
 
-public class Projectiles {
-	private int damage;
-	public Projectiles(int damage){
-		this.damage = damage;
+public class Projectiles extends Collision{
+	private int characterX;
+	private int characterY;
+	private int cursorX;
+	private int cursorY;
+	private Weapon bulletType;
+	
+	public int getCharacterX() {
+		return characterX;
 	}
-	public int getDamage() {
-		return damage;
+
+	public int getCharacterY() {
+		return characterY;
+	}
+
+	public int getCursorX() {
+		return cursorX;
+	}
+
+	public int getCursorY() {
+		return cursorY;
+	}
+
+	public Weapon getBulletType() {
+		return Weapon.getbulletType();
+	}
+
+	public Projectiles(int characterX, int characterY, int cursorX, int cursorY, Weapon bulletType){
+		this.characterX = characterX;
+		this.characterY = characterY;
+		this.cursorX = cursorX;
+		this.cursorY = cursorY;
+		this.bulletType = bulletType; //this needs more clarity because it has to be created
+	}
+
+	@Override
+	void collide() {
+		// TBD after discussing
+		
 	}
 }
