61c790258795e818ffd5dde74baf461c00a30cfe
mshahid5852 <mshahid5852@bths.edu>
2016-02-24 23:03:31 -0500

    Update EnemyDynamic.java

--- RPG8/src/project/story/EnemyDynamic.java
+++ RPG8/src/project/story/EnemyDynamic.java
@@ -1,5 +1,20 @@
 package project.story;
 
-public interface EnemyDynamic {
+import project.directors.Game;
+
+public class EnemyDynamic extends storyDemo{
+	
+	public void strengthen() {
+		Enemy.strength+=10;
+		System.out.println("enemy strength " + Enemy.strength);
+	}
+	public static void detectHit() {
+		if(Math.abs(Aya.getX()-Enemy.getX()) + Math.abs(Aya.getY()-Enemy.getY()) < 20)
+		{
+			strengthen();
+			Enemy.randomX();
+			Enemy.randomY();
+		}
+	}
 
 }
