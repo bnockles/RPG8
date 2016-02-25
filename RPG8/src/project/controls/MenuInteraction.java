a2f5319d86dce24d47da01c093c5525805e49100
Jacky <Jacky@Jacky-PC.si.rr.com>
2016-02-25 01:00:42 -0800

    YOLO

--- RPG8/src/project/controls/MenuInteraction.java
+++ RPG8/src/project/controls/MenuInteraction.java
@@ -1,5 +1,11 @@
 package project.controls;
 
+/** author Jacky */
+
 public interface MenuInteraction {
 
+	public void moveUp();
+	public void moveDown();
+	public void moveRight();
+	public void moveLeft();
 }
