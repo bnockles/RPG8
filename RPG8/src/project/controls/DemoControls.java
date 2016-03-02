a0621d1be8794ff08a74225c1c56d529004a7f45
shuu <Student8@10.8.33.129>
2016-03-02 13:41:15 -0500

    -can move two directions at once

--- RPG8/src/project/controls/DemoControls.java
+++ RPG8/src/project/controls/DemoControls.java
@@ -10,6 +10,7 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
 import javax.swing.SwingUtilities;
 
@@ -21,6 +22,10 @@ public class DemoControls extends project.directors.Screen implements KeyListene
 	BufferedImage landscape;
 	int x=300;
 	int y=300;
+	int walking=5;
+	int running=20;
+	int speed=walking;
+	ArrayList<Integer> moving;
 	String s = "Press 1 to select Game controls. Press 2 to select Menu Controls.";
 	String idle="Swarm is idle";
 	Boolean selection=false;
@@ -29,6 +34,7 @@ public class DemoControls extends project.directors.Screen implements KeyListene
 
 	public DemoControls(Game game) {
 		super(game);
+		moving = new ArrayList<Integer>();
 		// TODO Auto-generated constructor stub
 	}
 
@@ -67,7 +73,6 @@ public class DemoControls extends project.directors.Screen implements KeyListene
 				return;
 			}
 			s = "Swarm equipped his melee weapon";
-			update();
 		}
 		if(keyCode == KeyEvent.VK_2){
 			if(!selection){
@@ -77,124 +82,103 @@ public class DemoControls extends project.directors.Screen implements KeyListene
 				return;
 			}
 			s = "Swarm equipped his pistol";
-			update();
 		}
 		if(selection){
 			if(keyCode == KeyEvent.VK_W){
 				if(game){
 					s = "Swarm walked up";
-					y-=5;
+					if(!moving.contains(keyCode)) moving.add(keyCode);
 				}
 				if(menu){
 					s = "You selected the choice on the top";
 				}
-				update();
 			}
 			if(keyCode == KeyEvent.VK_A){
 				if(game){
 					s = "Swarm walked left";
-					x-=5;
+					if(!moving.contains(keyCode)) moving.add(keyCode);
 				}
 				if(menu){
 					s = "You selected the choice on the left";
 				}
-				update();
 			}
 			if(keyCode == KeyEvent.VK_S){
 				if(game){
 					s = "Swarm walked down";
-					y+=5;
+					if(!moving.contains(keyCode)) moving.add(keyCode);
 				}
 				if(menu){
 					s = "You selected the choice on the bottom";
 				}
-				update();
 			}
 			if(keyCode == KeyEvent.VK_D){
 				if(game){
 					s = "Swarm walked right";
-					x+=5;
+					if(!moving.contains(keyCode)) moving.add(keyCode);
 				}
 				if(menu){
 					s = "You selected the choice on the right";
 				}
-				update();
 			}
 		}
 		if(game){
 			if(keyCode == KeyEvent.VK_3){
 				s = "Swarm equipped his SMG";
-				update();
 			}
 			if(keyCode == KeyEvent.VK_4){
 				s = "Swarm equipped his rifle";
-				update();
 			}
 			if(keyCode == KeyEvent.VK_5){
 				s = "Swarm equipped his heavy";
-				update();
 			}
 			if(keyCode == KeyEvent.VK_6){
 				s = "Swarm equipped his rocket launcher";
-				update();
 			}
 			if(keyCode == KeyEvent.VK_G){
 				s = "Swarm equipped his explosive";
-				update();
 			}
 			if(keyCode == KeyEvent.VK_SHIFT){
 				s = "Swarm started to sprint";
-				update();
+				if(!moving.contains(moving.indexOf(keyCode))) moving.add(keyCode);
+				speed=running;
 			}
 			if(keyCode == KeyEvent.VK_E){
 				s = "Swarm interacted with something on the map";
-				update();
 			}
 			if(keyCode == KeyEvent.VK_R){
 				s = "Swarm reloaded his weapon";
-				update();
 			}
 			if(keyCode == KeyEvent.VK_F){
 				s = "Swarm went on stealth mode";
-				update();
 			}
 			if(keyCode == KeyEvent.VK_M){
 				s = "The player opens a transparent outline of the map";
-				update();
 			}
 			if(keyCode == KeyEvent.VK_ESCAPE){
 				s = "The player opens the exit menu";
-				update();
 			}
 		}
 		if(menu){
 			if(keyCode == 13){
 				s = "You have pressed Enter";
-				update();
 			};
 			if(keyCode == 16){
 				s = "You have pressed Shift";
-				update();
 			};
 			if(keyCode == 17){
 				s = "You have pressed Ctrl";
-				update();
 			};
 			if(keyCode == 18){
 				s = "You have pressed Alt";
-				update();
 			};
 			if(keyCode == 27){
 				s = "You have pressed Escape";
-				update();
 			};
 			if(keyCode == KeyEvent.VK_Z){
 				s = "You confirmed a selection";
-				update();
 			};
 			if(keyCode == KeyEvent.VK_X){
 				s = "You exited a screen";
-				update();
 			};
 		}
 	}
@@ -202,21 +186,17 @@ public class DemoControls extends project.directors.Screen implements KeyListene
 		if(game){
 			if(e.getButton()==MouseEvent.BUTTON1){
 				s = "Swarm fired his gun";
-				update();
 			}
 			if(e.getButton()==MouseEvent.BUTTON3){
 				s = "Swarm fired his alternative shot";
-				update();
 			}
 		}
 		if(menu){
 			if(e.getButton()==MouseEvent.BUTTON1){
 				s = "You left clicked confirm";
-				update();
 			}
 			if(e.getButton()==MouseEvent.BUTTON3){
 				s = "You right clicked an exitted a menu";
-				update();
 			}
 		}
 	}
@@ -226,21 +206,14 @@ public class DemoControls extends project.directors.Screen implements KeyListene
 		// TODO Auto-generated method stub
 		int keyCode = e.getKeyCode();
 		if(game){
-			if(keyCode == KeyEvent.VK_W){
+			if(keyCode==KeyEvent.VK_W||keyCode==KeyEvent.VK_A||keyCode==KeyEvent.VK_S||keyCode==KeyEvent.VK_D){
 				s = idle;
-				update();
+				moving.remove(moving.indexOf(keyCode));
 			}
-			if(keyCode == KeyEvent.VK_A){
-				s = idle;
-				update();
-			}
-			if(keyCode == KeyEvent.VK_S){
-				s = idle;
-				update();
-			}
-			if(keyCode == KeyEvent.VK_D){
-				s = idle;
-				update();
+			if(keyCode==KeyEvent.VK_SHIFT){
+				moving.remove(moving.indexOf(keyCode));
+				speed=walking;
+				s="Swarm stopped sprinting";
 			}
 		}
 	}
@@ -258,8 +231,17 @@ public class DemoControls extends project.directors.Screen implements KeyListene
 
 	@Override
 	public void paintScreen(Graphics2D g2) {
+		checkMotion();
 		g2.setColor(Color.blue);
 		g2.drawString(s,x, y);
+	}
+
+	private void checkMotion() {
+		// TODO Auto-generated method stub
+		if(moving.contains(KeyEvent.VK_W)) y-=speed;
+		if(moving.contains(KeyEvent.VK_A)) x-=speed;
+		if(moving.contains(KeyEvent.VK_S)) y+=speed;
+		if(moving.contains(KeyEvent.VK_D)) x+=speed;
 	}
 
 	@Override
