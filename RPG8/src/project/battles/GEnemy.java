8330275649845fafd0667bae1c31444fbd28e7ad
Andy Zheng <azheng4119@bths.edu>
2016-03-03 20:08:46 -0500

    added AI to GEnemy

--- RPG8/src/project/battles/GEnemy.java
+++ RPG8/src/project/battles/GEnemy.java
@@ -7,12 +7,12 @@ import project.items.Weapon;
 import project.save.ItemState;
 
 public class GEnemy extends EnemyAI{
-	
+
 	public GEnemy(BufferedImage[][] images, int[] stats, int[] vision, Weapon weapon, int type){
 		super(images,stats,vision,weapon,type);
 		this.weapon = weapon;
 	}
- 
+
 	public GEnemy(BufferedImage[][] images, int[] stats,int[] vision, Weapon weapon, int type, boolean[] conditions){
 		super(images,stats,vision,weapon,type);
 		this.targetLock = conditions[0];
@@ -21,6 +21,20 @@ public class GEnemy extends EnemyAI{
 		this.boss = conditions[3];
 	}
 
+	public void distanceAway(){
+		float dist = (float) Math.sqrt(
+				Math.pow(spawnedX - spawnedY, 2) +
+				Math.pow(x - y, 2) );
+		if (dist > 100) backToSpawn();
+	}
+
+	public void backToSpawn(){
+		int  a = spawnedX - x;
+		int b = spawnedY - y;
+		x+= BattlesScreen.GE_SPEED * (Math.signum(a));
+		y+= BattlesScreen.GE_SPEED * (Math.signum(b));
+
+	}
 	@Override
 	protected void reaction() {
 		// TODO Auto-generated method stub
@@ -40,7 +54,7 @@ public class GEnemy extends EnemyAI{
 		else {
 			x--;
 		}
-		
+
 		if (distanceY >= 0) {
 			y++;
 		}
