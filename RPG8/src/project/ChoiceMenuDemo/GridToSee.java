ef29ee0a1f0759f1e9bad526e8fe560e9d08815b
Maxwell <Maxwell@Max-PC.home>
2016-03-04 00:48:23 -0500

    Finished colors I felt like doing

--- RPG8/src/project/ChoiceMenuDemo/GridToSee.java
+++ RPG8/src/project/ChoiceMenuDemo/GridToSee.java
@@ -22,14 +22,18 @@ import project.ChoiceMenuDemo.MenuTheme;
  */
 
 public class GridToSee extends Screen implements KeyListener{
-
-	boolean choices = false; //controls if control over main or sub menu
+	
+	
+	public static final int MAIN_GRID = 0;
+	public static final int MINI_SUB = 1;
+	public static final int THEME_MENU = 2;
+	
+	int choices = MAIN_GRID; //controls if control over main or sub menu
+	
 	public int highl = 0;
 	
 	ArrayList<ChoiceDesc> choice;
 	
-	
-
 	Font def = new Font ("Helvetica", Font.BOLD, 30);
 	Font smallerDef = new Font("Helvetica", Font.BOLD, 20);
 	int x = 123;
@@ -38,11 +42,12 @@ public class GridToSee extends Screen implements KeyListener{
 	int x2 = 0;
 	int y2 = 0;
 	int numberOfBoxes = 0;
+	int selectMem = 0;
 	
 	
 	public GridToSee(Game game) {
 		super(game);
-
+		MenuTheme.setTheme(1);
 		this.choice = new ArrayList<ChoiceDesc>();
 		choice.add(new ChoiceDesc("Box 1", new File("resources/samusr.jpg"), "Go left to go to end", ""));
 		choice.add(new ChoiceDesc("Box 2", new File("resources/image1.jpg"), "PROPERTY OF MAX", ""));
@@ -78,62 +83,62 @@ public class GridToSee extends Screen implements KeyListener{
 		}
 		
 		if(e.getKeyCode()== KeyEvent.VK_SPACE){
-			choices = true;
-			highl = 0;
+			if (choices == MAIN_GRID){
+				selectMem = highl;
+				highl = 0;
+			}
+			if (choices == MINI_SUB){
+				
+			}
+			choices = MINI_SUB;
 			update();
 		}	
 		if(e.getKeyCode() == KeyEvent.VK_BACK_SPACE){
 			this.game.setScreen(new MainMenuScreen(this.game));
 		}
-	
+		if(e.getKeyCode()== KeyEvent.VK_C){
+			if (choices == MINI_SUB){
+				choices = MAIN_GRID;
+				highl = selectMem;
+			}	
+		}
+		if(e.getKeyCode()== KeyEvent.VK_D){
+			this.game.setScreen(new GridToSee(this.game));
+		}	
+		
 	}
 	
 	public void paintScreen(Graphics2D g2) {
-		if (choices == false) {
+		if (choices == MAIN_GRID) {
 			numberOfBoxes = 6;
 		}
 		else {
 			numberOfBoxes = 4;
 		}
 		
-		g2.setColor(Color.GRAY);
-		g2.fillRect(0, 0, width, height);
-		
-		g2.setColor(Color.black);
-		
-		//The 6 grid boxes
-		g2.fillRect(125, 130, 230, 230);
-		g2.fillRect(385, 130, 230, 230);
-		g2.fillRect(645, 130, 230, 230);
-		g2.fillRect(125, 440, 230, 230);
-		g2.fillRect(385, 440, 230, 230);
-		g2.fillRect(645, 440, 230, 230);
-		
-		
-		
 		if (highl == 0){
 			x = 123;
 			y = 128;
-			x2 = 85;
-			y2 = 115;
+			x2 = 125;
+			y2 = 265;
 		}
 		if (highl == 1){
 			x = 383;
 			y = 128;
-			x2 = 585;
-			y2 = 115;
+			x2 = 265;
+			y2 = 265;
 		}
 		if (highl == 2){
 			x = 643;
 			y = 128;
-			x2 = 85;
-			y2 = 515;
+			x2 = 125;
+			y2 = 405;
 		}	
 		if (highl == 3){
 			x = 123;
 			y = 438;
-			x2 = 585;
-			y2 = 515;
+			x2 = 265;
+			y2 = 405;
 		}
 		if (highl == 4){
 			x = 383;
@@ -144,22 +149,35 @@ public class GridToSee extends Screen implements KeyListener{
 			y = 438;
 		}
 		
-	
-		if (choices  == false){
-			g2.setColor(selectBlue); 
+
+		if (choices  == MAIN_GRID){
+			g2.setColor(MenuTheme.bR);
+			g2.fillRect(0, 0, width, height);
+			
+			g2.setColor(MenuTheme.boxColor);
+			
+			//The 6 grid boxes
+			g2.fillRect(125, 130, 230, 230);
+			g2.fillRect(385, 130, 230, 230);
+			g2.fillRect(645, 130, 230, 230);
+			g2.fillRect(125, 440, 230, 230);
+			g2.fillRect(385, 440, 230, 230);
+			g2.fillRect(645, 440, 230, 230);
+			
+			//THE SELECTION BOX
+			g2.setColor(MenuTheme.selectColor); 
 			g2.fillRect(x, y, w, w); //its a square anyway
-		}
-		
-		g2.setFont(def);
-		g2.setColor(Color.white);
-		g2.drawString(choice.get(0).name, 128, 160);
-		g2.drawString(choice.get(1).name, 388, 160);
-		g2.drawString(choice.get(2).name, 648, 160);
-		g2.drawString(choice.get(3).name, 128, 470);
-		g2.drawString(choice.get(4).name, 388, 470);
-		g2.drawString(choice.get(5).name, 648, 470);
-		
-		if (choices == false){
+			
+			//FILLING IN THE INFO ON BOXES
+			g2.setFont(def);
+			g2.setColor(MenuTheme.textColor);
+			g2.drawString(choice.get(0).name, 128, 160);
+			g2.drawString(choice.get(1).name, 388, 160);
+			g2.drawString(choice.get(2).name, 648, 160);
+			g2.drawString(choice.get(3).name, 128, 470);
+			g2.drawString(choice.get(4).name, 388, 470);
+			g2.drawString(choice.get(5).name, 648, 470);
+			
 			//only selected box shows image
 			BufferedImage boxImg = null;
 			try {
@@ -173,21 +191,37 @@ public class GridToSee extends Screen implements KeyListener{
 			
 		}
 		
-		if (choices == true){
+		if (choices == MINI_SUB){
+			g2.setFont(def);
 			int w2 = 50;
 			
-			g2.setColor(Color.darkGray); //background of submenu
-			g2.fillRect(10, 40, 750, 600);
+			g2.setColor(MenuTheme.subBr); //background of submenu
+			g2.fillRect(0, 0, width, height);
 			
-			g2.setColor(selectBlue); //Selection color
+			g2.setColor(MenuTheme.selectColor); //Selection color
 			g2.fillRect(x2, y2, w2, w2);
 			
+			g2.fillRect(500, 250, 230, 230); //SELECTED BOX FROM GRID
+			g2.setColor(MenuTheme.textColor);
+			g2.drawString(choice.get(selectMem).name, 503, 280);
+			BufferedImage boxImg = null;
+			try {
+				boxImg = ImageIO.read(choice.get(selectMem).pic);
+			} catch (IOException e) {
+			}
+			g2.drawImage(boxImg, 506, 300, 120, 120, null);
+			g2.setFont(smallerDef);
+			g2.drawString(choice.get(selectMem).des, 506, 450);
 			
 			g2.setColor(Color.white);
-			g2.drawString("Y", 100, 150);
-			g2.drawString("N", 600, 150);
-			g2.drawString("D", 100, 550);
-			g2.drawString("C", 600, 550);
+			g2.setFont(def);
+			g2.drawString("Y", 140, 300);
+			g2.drawString("N", 280, 300);
+			g2.drawString("D", 140, 440);
+			g2.drawString("C", 280, 440);
+		}
+		if (choices == THEME_MENU){
+			
 		}
 	}
 	@Override
--- RPG8/src/project/ChoiceMenuDemo/MenuTheme.java
+++ RPG8/src/project/ChoiceMenuDemo/MenuTheme.java
@@ -4,38 +4,43 @@ import java.awt.Color;
 
 public class MenuTheme {
 	
-	private String bR;
-	private String boxColor;
-	private String selectColor;
-	private String textColor;
-	private String desc;
-	private String subBr;
+	public static Color bR;
+	public static Color boxColor;
+	public static Color selectColor;
+	public static Color textColor;
+	public static String desc;
+	public static Color subBr;
 	
-	public Color selectBlue = new Color (30, 60, 100); //Selection color
+	//added custom colors
+	public static Color selectBlue = new Color (30, 60, 100); //Selection color
+	public static Color backgroundGreen = new Color (17, 180, 82);
+	public static Color selectGreen = new Color (14, 142, 65);
+	public static Color greenText = new Color (130, 161, 142);
+	public static Color greenBR = new Color (62,116, 83);
 	
-	public MenuTheme(String bR, String boxColor, String selectColor, String textColor, String subBr, String desc){
-		this.bR = bR;
-		this.boxColor = boxColor;
-		this.selectColor = selectColor;
-		this.subBr = subBr;
-		this.textColor = textColor;
-		this.desc = desc;
-	}
-	public static final int DEFAULT_THEME = 0; //default dark grey, black , blue
-	public static final int GREEN_THEME = 1; //green green green
-	public static final int WATER_THEME = 2; //navy blue, aqua, teal
-	public static final int SUNNY_THEME = 3; //red, orange , yellow
 	
-	public void getTheme(int theme){
-		if (theme == DEFAULT_THEME){
-			bR = "GRAY";
-			boxColor = "black";
-			selectColor = "selectBlue";
-			textColor = "white";
-			subBr = "darkGray";
+	
+	//public static final int DEFAULT_THEME = 0; //default dark grey, black , blue
+	//public static final int GREEN_THEME = 1; //green green green
+	//public static final int WATER_THEME = 2; //navy blue, aqua, teal
+	//public static final int SUNNY_THEME = 3; //red, orange , yellow
+	
+	public static void setTheme(int theme){
+		if (theme == 0){
+			bR = Color.GRAY;
+			boxColor = Color.black;
+			selectColor = selectBlue;
+			textColor = Color.white;
+			subBr = Color.darkGray;
 			desc = "Default Color Scheme";
-		
 		}
-		
+		if (theme == 1){
+			bR = backgroundGreen;
+			boxColor = selectGreen;
+			selectColor = Color.GREEN;
+			textColor = greenText;
+			subBr = greenBR;
+			desc = "A Green Theme";
+		}
 	}
 }
