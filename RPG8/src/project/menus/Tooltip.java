7021ced942945d7c727079b18c2e3e20aeeb3a82
Mark Mozgovoy <mmozgovoy7464@bths.edu>
2016-03-03 22:33:41 -0500

    Finished making instances and updated demo

--- RPG8/src/project/menus/Tooltip.java
+++ RPG8/src/project/menus/Tooltip.java
@@ -24,6 +24,8 @@ public class Tooltip extends VerticalMenu{
 	private String title;
 	private File file;
 	private String description;
+	private int buyPrice;
+	private int sellPrice;
 	
 	public Tooltip(int width, int height, int x, int y, String title, File file, String description, int imgX, int imgY) {
 		super(width, height);
@@ -36,6 +38,31 @@ public class Tooltip extends VerticalMenu{
 		this.file = file;
 		this.imgX = imgX;
 		this.imgY = imgY;
+	}
+	
+	public Tooltip(int width, int height, int x, int y, String title, File file, String description, int imgX, int imgY, int buyPrice, int sellPrice) {
+		super(width, height);
+		this.width = width;
+		this.height = height;
+		this.description = description;
+		this.x = x;
+		this.y = y;
+		this.title = title;
+		this.file = file;
+		this.imgX = imgX;
+		this.imgY = imgY;
+		this.buyPrice = buyPrice;
+		this.sellPrice = sellPrice;
+	}
+	
+	public Tooltip(int width, int height, int x, int y, String title, String description) {
+		super(width, height);
+		this.width = width;
+		this.height = height;
+		this.description = description;
+		this.x = x;
+		this.y = y;
+		this.title = title;
 	}
 	
 	public String getTitle() {
@@ -77,14 +104,6 @@ public class Tooltip extends VerticalMenu{
 	public void setDescription(String description) {
 		this.description = description;
 	}
-
-	public Tooltip(int width, int height, int x, int y, String title, String description) {
-		super(width, height);
-		this.x = x;
-		this.y = y;
-		this.title = title;
-		this.description = description;
-	}
 	
 	public void setWidth(int width) {
 		this.width = width;
@@ -123,10 +142,12 @@ public class Tooltip extends VerticalMenu{
 		//A tooltip doesn't have selections. Other menus will take care of this.
 	}
 
-	public void drawTooltip(Graphics2D g2){
+	public void drawTooltipInventory(Graphics2D g2){
 		g2.setColor(Color.black);
 		g2.fillRect(this.getX(), this.getY(), this.getWidth(), this.getHeight());
 		g2.setColor(Color.yellow);
+		Font font1 = new Font("Courier", Font.ROMAN_BASELINE, 50);
+		g2.setFont(font1);
 		g2.drawString(this.getTitle(), this.getX()+110, this.getY()+60);
 		BufferedImage img = new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB);
 		try{
@@ -140,4 +161,58 @@ public class Tooltip extends VerticalMenu{
 		g2.drawString(this.getDescription(), this.getX()+50, this.getY()+200);
 	}
 	
+	public void drawTooltipStore(Graphics2D g2) {
+		g2.setColor(Color.black);
+		g2.fillRect(this.getX(), this.getY(), this.getWidth(), this.getHeight());
+		g2.setColor(Color.yellow);
+		Font font1 = new Font("Courier", Font.ROMAN_BASELINE, 50);
+		g2.setFont(font1);
+		g2.drawString(this.getTitle(), this.getX()+110, this.getY()+60);
+		BufferedImage img = new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB);
+		try{
+			img = ImageIO.read(this.getFile());
+		}
+		catch(IOException e){};
+		g2.drawImage(img, null, this.getX()+185, this.getY()+100);
+		Font font2 = new Font("Courier", Font.ROMAN_BASELINE, 25);
+		g2.setFont(font2);
+		g2.setColor(Color.white);
+		g2.drawString(this.getDescription(), this.getX()+50, this.getY()+200);
+		g2.drawString("Buy Price: $"+this.getBuyPrice()+"", this.getX()+50, this.getY()+230);
+		g2.drawString("Sell Price: $"+this.getSellPrice()+"", this.getX()+50, this.getY()+260);
+	}
+	
+	public void drawTooltipDialogue(Graphics2D g2) {
+		g2.setColor(Color.black);
+		g2.fillRect(this.getX(), this.getY(), this.getWidth(), this.getHeight());
+		g2.setColor(Color.yellow);
+		Font font1 = new Font("Courier", Font.ROMAN_BASELINE, 30);
+		g2.setFont(font1);
+		g2.drawString(this.getTitle() + " says...", this.getX()+300, this.getY()+60);
+		Font font2 = new Font("Courier", Font.ROMAN_BASELINE, 25);
+		g2.setFont(font2);
+		g2.setColor(Color.white);
+		g2.drawString(this.getDescription(), this.getX()+50, this.getY()+105);
+	}
+
+	public int getBuyPrice() {
+		return buyPrice;
+	}
+
+	public int getSellPrice() {
+		return sellPrice;
+	}
+
+	public void setFile(File file) {
+		this.file = file;
+	}
+
+	public void setBuyPrice(int buyPrice) {
+		this.buyPrice = buyPrice;
+	}
+
+	public void setSellPrice(int sellPrice) {
+		this.sellPrice = sellPrice;
+	}
+
 }
--- RPG8/src/project/menus/TooltipOptions.java
+++ RPG8/src/project/menus/TooltipOptions.java
@@ -12,11 +12,12 @@ public class TooltipOptions {
 	public static final int TOOLTIP_DIALOGUE = 2;
 	
 	public static Tooltip getTooltip(int type){
-		if(type == TOOLTIP_INVENTORY) return new Tooltip(450, 230, 492, 220,
+		if(type == TOOLTIP_INVENTORY) return new Tooltip(450, 230, 492, 90,
 				"Title Here", new File("resources/image1.jpg"), "Description here",
-				234, 120);
-		if(type == TOOLTIP_STORE) return null;
-		if(type == TOOLTIP_DIALOGUE) return null;
+				234, -10);
+		if(type == TOOLTIP_STORE) return new Tooltip(450, 290, 492, 330, "Title Here", new File("resources/image1.jpg"), "Description here",
+				317, 660, 100, 50);
+		if(type == TOOLTIP_DIALOGUE) return new Tooltip(870, 230, 72, 630, "Character Name Here", "Dialogue Text Here");
 		
 		//If you do not put in a valid type, it will just return the default option.
 		return new Tooltip(450, 230, 492, 220,
