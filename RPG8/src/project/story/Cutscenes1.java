187aa922b417718173cf35b78238cb971c59a425
jiawei <jiawei@192.168.1.128>
2016-03-07 17:55:08 -0800

    00

--- RPG8/src/project/story/Cutscenes1.java
+++ RPG8/src/project/story/Cutscenes1.java
@@ -1,9 +1,16 @@
 package project.story;
 
 import java.awt.Color;
+
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+
+
+import java.awt.RenderingHints;
+import java.awt.geom.Ellipse2D;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 @SuppressWarnings("serial")
 public abstract class Cutscenes1 extends Cutscenes {
 	static int acceptableX;
@@ -22,14 +29,17 @@ public abstract class Cutscenes1 extends Cutscenes {
 		if(acceptableX >= 0 && acceptableX <= 100 && acceptableY >= 0 && acceptableY <= 100){ 
 			//code to make cutscene here
 			
-		
-			BufferedImage image=new BufferedImage(1000, 800, BufferedImage.TYPE_INT_ARGB);
+	
+			/**
+			= (Graphics2D) image.getGraphics();
+	//works
+			**///BufferedImage image=new BufferedImage(1000, 800, BufferedImage.TYPE_INT_ARGB);
 			
-			Graphics2D g2 = (Graphics2D) image.getGraphics();
+			Graphics2D g2 = null ;
 			g2.setColor(Color.black);
 			g2.drawRect(250, 200, 200,100);
 			g2.drawString("Hello my goodman", 275, 250);
-	
+		
 			
 			
 			storyDemo.setCutscene(true);
