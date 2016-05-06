6f4cdc86c58ae1a7ba66c7bd8fd3c582dea26fb0
terribleC <wongandrew21@gmail.com>
2016-05-06 14:30:11 -0400

    Update and rename OverworldInterface.java to BattleInterface.java

--- RPG8/src/project/overworld/BattleInterface.java
+++ RPG8/src/project/overworld/BattleInterface.java
@@ -2,7 +2,7 @@ package project.overworld;
 
 import java.util.ArrayList;
 
-public interface OverworldInterface {//yifan use this in battlescreen -->psintscrene--> 4 var --> for loop--> access these objects: x.get(0).printBushes(); 
+public interface BattleInterface {//yifan use this in battlescreen -->psintscrene--> 4 var --> for loop--> access these objects: x.get(0).printBushes(); 
 	public ArrayList<Boundaries> getBoundaries(int n);
 	public ArrayList<Obstacle> getObstacles(int n);
 	public ArrayList<Region> getRegion(int n);
