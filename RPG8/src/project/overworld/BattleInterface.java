483a4f602879a6e8fe644be3b4ab83a5d98b21c1
Student8 <Student8@10.8.33.106>
2016-05-10 13:04:48 -0400

    d

--- RPG8/src/project/overworld/BattleInterface.java
+++ RPG8/src/project/overworld/BattleInterface.java
@@ -3,7 +3,8 @@ package project.overworld;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 
-public interface BattleInterface {//use to convert your battle to a map screen
-	public BufferedImage getBackgroundImage();
-	public int getEnemyType();
+public interface BattleInterface {
+	
+	public BufferedImage getBackgroundImage();//gives a background image
+	public int getEnemyType();//gives enemy type based on image
 }
