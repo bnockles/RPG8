5fc3521b77b72d0c996371f7d12fd2df4ba9e674
Wilson W <swat2129@bths.edu>
2016-05-15 11:27:46 -0400

    New save

--- RPG8/src/project/save/BattleSave.java
+++ RPG8/src/project/save/BattleSave.java
@@ -1,9 +1,13 @@
 package project.save;
 
-public class BattleSave extends Save {
+import java.util.ArrayList;
 
-	public BattleSave(String name) {
-		super(name);
+public class BattleSave extends Save{
+	public static void saveData(String id, ArrayList<Integer> data) {
+		t.put(id, data);
 	}
-
+	
+	public static ArrayList<Integer> getIntArrayList(String id) {
+		return (ArrayList<Integer>) t.get(id);
+	}
 }
---
+++ RPG8/src/project/save/ChooseSave.java
@@ -0,0 +1,29 @@
+package project.save;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ChooseSave {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Pick a save file: ");
+		Save.chooseSave(in.nextInt());
+		
+		Save.saveData("something", "A String!");
+		Save.saveData("test2", 3.14);
+		
+		ArrayList<Integer> testArr = new ArrayList<Integer>();
+		testArr.add(1);
+		testArr.add(2);
+		testArr.add(3);
+		testArr.add(4);
+		
+		BattleSave.saveData("testArr", testArr);
+		
+		System.out.println("Testing load: " + Save.getDouble("test2"));
+		ArrayList<Integer> resultArr = BattleSave.getIntArrayList("testArr");
+		System.out.println(Arrays.toString(resultArr.toArray()));
+	}
+}
