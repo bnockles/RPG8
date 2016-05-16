4e351547b1390b34cf829246fb1a6cf257e6c817
Mark Mozgovoy <mmozgovoy7464@bths.edu>
2016-05-16 12:52:05 -0400

    Fixed interface for Mohammed. Interface will take in a string and return an int that will be the starting point of the centered string.

--- RPG8/src/project/menus/StringCenterer.java
+++ RPG8/src/project/menus/StringCenterer.java
@@ -2,6 +2,6 @@ package project.menus;
 
 public interface StringCenterer {
 
-	public int centerString(String s);
+	public int centerString(String s, int x, int width);//returns an int for a place where the string will begin. 
 	
 }
