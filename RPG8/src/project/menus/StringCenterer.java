4fd669ddc3f4c5ff7a0d7ff8226d10cc95ff3b4b
Mark Mozgovoy <mmozgovoy7464@bths.edu>
2016-05-16 13:07:15 -0400

    Added comments

--- RPG8/src/project/menus/StringCenterer.java
+++ RPG8/src/project/menus/StringCenterer.java
@@ -1,7 +1,13 @@
 package project.menus;
 
+/**
+ * 
+ * @author Mark Mozgovoy
+ *
+ */
+
 public interface StringCenterer {
 
 	public int centerString(String s, int x, int width, int widthOfCharacter);//returns an int for a place where the string will begin. 
-	
+	//for Mohammed.
 }
