7ff2db9f7ed3318907f8d6ccc55428a0e2f9895b
Melvin <Student8@10.8.33.119>
2016-05-16 14:01:11 -0400

    interface change

--- RPG8/src/project/battles/CheatCodeDeterminer.java
+++ RPG8/src/project/battles/CheatCodeDeterminer.java
@@ -2,7 +2,9 @@ package project.battles;
 
 import java.awt.event.KeyEvent;
 
+import project.controls.cheatCodeInterface;
+
 public interface CheatCodeDeterminer {
-	public void determineValidCheatCode(KeyEvent e);
+	public void determineValidCheatCode(KeyEvent e, cheatCodeInterface battles);
 										//cheatCodeInterface battles
 }
