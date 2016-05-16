dbd79a56b2fa98412af4dc706a0cd09e2a21d161
Wilson W <swat2129@bths.edu>
2016-05-15 20:49:11 -0400

    Tested loadig saves

--- RPG8/src/project/save/ChooseSave.java
+++ RPG8/src/project/save/ChooseSave.java
@@ -1,18 +1,24 @@
 package project.save;
 
+/*
+ * @author Wilson Wat
+ * 
+ */
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class ChooseSave {
+	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
 		System.out.println("Pick a save file: ");
 		Save.chooseSave(in.nextInt());
 		
-		Save.saveData("something", "A String!");
-		Save.saveData("test2", 3.14);
+		// Save.saveData("something", "A String!");
+		// Save.saveData("test2", 3.14);
 		
 		ArrayList<Integer> testArr = new ArrayList<Integer>();
 		testArr.add(1);
@@ -20,9 +26,12 @@ public class ChooseSave {
 		testArr.add(3);
 		testArr.add(4);
 		
-		BattleSave.saveData("testArr", testArr);
+		// BattleSave.saveData("testArr", testArr);
+		// BattleSave.saveData();
 		
+		System.out.println("Testing load: " + Save.getString("something"));
 		System.out.println("Testing load: " + Save.getDouble("test2"));
+		
 		ArrayList<Integer> resultArr = BattleSave.getIntArrayList("testArr");
 		System.out.println(Arrays.toString(resultArr.toArray()));
 	}
