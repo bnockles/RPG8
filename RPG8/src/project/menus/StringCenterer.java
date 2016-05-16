64edf15fca233bc0a6e5044e8fdb428c1a3e81fb
Mark Mozgovoy <mmozgovoy7464@bths.edu>
2016-05-16 12:59:50 -0400

    Added parameter to interface

--- RPG8/src/project/menus/StringCenterer.java
+++ RPG8/src/project/menus/StringCenterer.java
@@ -2,6 +2,6 @@ package project.menus;
 
 public interface StringCenterer {
 
-	public int centerString(String s, int x, int width);//returns an int for a place where the string will begin. 
+	public int centerString(String s, int x, int width, int widthOfCharacter);//returns an int for a place where the string will begin. 
 	
 }
