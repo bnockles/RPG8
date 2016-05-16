e9576bc004af71093d226a029a3fae249b549fed
Wilson W <swat2129@bths.edu>
2016-05-16 13:20:11 -0400

    Updated SaveScreen.java

--- RPG8/src/project/save/SaveScreen.java
+++ RPG8/src/project/save/SaveScreen.java
@@ -1,16 +1,28 @@
 package project.save;
 
+import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import project.directors.Game;
 import project.directors.Screen;
+import project.directors.UtilityMethods;
 
-public class SaveScreen extends Screen {
+public class SaveScreen extends Screen implements KeyListener {
 
 	public SaveScreen(Game game) {
 		super(game);
 		// TODO Auto-generated constructor stub
+	}
+	
+	public void paintScreen(Graphics2D g2) {
+		// TODO Auto-generated method stub
+		g2.setColor(Color.LIGHT_GRAY);
+		g2.fillRect(0, 0, width, height);
+		
+		String startText = "Press Space Bar to start";
+		UtilityMethods.centerText(g2, startText, width, height);
 	}
 
 	@Override
@@ -20,7 +32,19 @@ public class SaveScreen extends Screen {
 	}
 
 	@Override
-	public void paintScreen(Graphics2D g2) {
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
