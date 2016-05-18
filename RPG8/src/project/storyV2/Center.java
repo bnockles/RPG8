6c51821cbc877ffe73644bda9c054dd22e67e423
mshahid5852 <mshahid5852@bths.edu>
2016-05-18 14:00:10 -0400

    Update Center.java
    dh

--- RPG8/src/project/storyV2/Center.java
+++ RPG8/src/project/storyV2/Center.java
@@ -1,7 +1,7 @@
 public class Center implements StringCenterer {
 	public int centerString(String s, int whereBoxBeginsX, int widthOfBox, int widthOfEachCharacterInPixels) {
                 int x = (widthOfBox-widthOfEachCharacterInPixels)/2;
-                return x+whereBoxBegins;
+                return x+whereBoxBeginsX;
 
         }
 
