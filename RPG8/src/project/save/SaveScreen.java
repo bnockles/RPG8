2811c0dce7cafd6c52a6b4e9c72bbedfc667aa61
Wilson W <swat2129@bths.edu>
2016-05-18 12:49:21 -0400

    Made basic GUI outline for SaveDemo.java

--- RPG8/src/project/save/SaveScreen.java
+++ RPG8/src/project/save/SaveScreen.java
@@ -1,6 +1,7 @@
 package project.save;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -17,8 +18,21 @@ public class SaveScreen extends Screen implements KeyListener {
 	
 	@Override
 	public void paintScreen(Graphics2D g2) {
-		g2.setColor(Color.black);
-		g2.drawString("This Demo is working!", 30, 75);
+		// Header
+		Font sansSerif = new Font("Helvetica", Font.PLAIN, 60);
+		g2.setColor(new Color(44, 62, 80));
+		g2.fillRect(0, 0, width, height);
+		
+		g2.setColor(new Color(149, 165, 166));
+		g2.fillRect(100, 100, 100, 100);
+		
+		g2.setFont(sansSerif);
+		g2.drawString("Save Selection", 220, 200);
+		
+		for (int i = 1; i< 4; i++) {
+			g2.fillRect(100, 175 * i + 75, width - 200, 100);
+			g2.fillRect(100, 175 * i + 72 + 2, width - 200, 100);
+		}
 	}
 
 	@Override
