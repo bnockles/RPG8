2e09e217255e029ac90a8d878df011de4ba29c41
mshahid5852 <mshahid5852@bths.edu>
2016-05-19 20:37:51 -0400

    g
    h

--- RPG8/src/project/storyV2/chatConvo.java
+++ RPG8/src/project/storyV2/chatConvo.java
@@ -1,4 +1,15 @@
 
 public class chatConvo {
-
+	String charName;
+	String dialogue;
+	public chatConvo(String charName, String dialogue) {
+		this.charName = charName;
+		this.dialogue = dialogue;
+	}
+	public String getName() {
+		return charName;
+	}
+	public String getDialogue() {
+		return dialogue;
+	}
 }
