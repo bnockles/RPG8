025d78918a6ce86a8e87587b173fc4ca70d9a41e
Student8 <Student8@10.8.33.100>
2016-05-20 14:03:37 -0400

    sdff

--- RPG8/src/project/items/Ammo.java
+++ RPG8/src/project/items/Ammo.java
@@ -113,76 +113,76 @@ public class Ammo extends Item{
 		
 		
 		
-		if(eff == ItemResources.FIRE){
-			if(!target.isBurned){
-				final Timer timer = new Timer();
-				final TimerTask applyBurn = new TimerTask(){
-					public void run(){
-						if(burnDuration>=5){
-							burnDuration=0;
-							target.isBurned=false;
-							timer.cancel();
-						}else{
-							System.out.println("BURNING");
-							target.health -=5;
-							if(target.health<0){
-								target.health=0;
-							}
-							burnDuration++;
-						}
-					}
-				};
-				timer.scheduleAtFixedRate(applyBurn, 1000, 1000);
-				target.isBurned=true;
-			}	
-		}
-		if(eff == ItemResources.CORROSIVE){
-			if(!target.isCorrosive){
-				target.isCorrosive=true;
-				final Timer timer = new Timer();
-				final TimerTask applyCorrosive = new TimerTask(){
-					public void run(){
-						System.out.println("CORR");
-						if(corrodeDuration==0){
-							
-							target.armor/=2;
-							System.out.println("Corroded Armor" + target.armor);
-							
-						}
-						if(corrodeDuration>=4){
-							target.armor*=2;
-							System.out.println("Normal Armor" + target.armor);
-							corrodeDuration=0;
-							target.isCorrosive=false;
-							timer.cancel();
-							System.out.println("now:"+target.name + " Armor is: " + target.armor);
-						}
-						corrodeDuration++;
-						if(target.health<0){
-							target.health=0;
-						}
-					}
-				};
-				timer.scheduleAtFixedRate(applyCorrosive, 0, 1000);
-			}
-		}
-		if(eff==ItemResources.STUN){
-			target.isStunned=true;
-			final Timer timer = new Timer();
-			final TimerTask applyStun = new TimerTask(){
-				public void run(){
-					System.out.println("STUNNED");
-					if(stunDuration>=3){
-						stunDuration=0;
-						target.isStunned=false;
-						timer.cancel();
-					}else{
-						stunDuration++;
-					}
-				}
-			};
-			timer.scheduleAtFixedRate(applyStun, 1000, 1000);
-		}
+//		if(eff == ItemResources.FIRE){
+//			if(!target.isBurned){
+//				final Timer timer = new Timer();
+//				final TimerTask applyBurn = new TimerTask(){
+//					public void run(){
+//						if(burnDuration>=5){
+//							burnDuration=0;
+//							target.isBurned=false;
+//							timer.cancel();
+//						}else{
+//							System.out.println("BURNING");
+//							target.health -=5;
+//							if(target.health<0){
+//								target.health=0;
+//							}
+//							burnDuration++;
+//						}
+//					}
+//				};
+//				timer.scheduleAtFixedRate(applyBurn, 1000, 1000);
+//				target.isBurned=true;
+//			}	
+//		}
+//		if(eff == ItemResources.CORROSIVE){
+//			if(!target.isCorrosive){
+//				target.isCorrosive=true;
+//				final Timer timer = new Timer();
+//				final TimerTask applyCorrosive = new TimerTask(){
+//					public void run(){
+//						System.out.println("CORR");
+//						if(corrodeDuration==0){
+//							
+//							target.armor/=2;
+//							System.out.println("Corroded Armor" + target.armor);
+//							
+//						}
+//						if(corrodeDuration>=4){
+//							target.armor*=2;
+//							System.out.println("Normal Armor" + target.armor);
+//							corrodeDuration=0;
+//							target.isCorrosive=false;
+//							timer.cancel();
+//							System.out.println("now:"+target.name + " Armor is: " + target.armor);
+//						}
+//						corrodeDuration++;
+//						if(target.health<0){
+//							target.health=0;
+//						}
+//					}
+//				};
+//				timer.scheduleAtFixedRate(applyCorrosive, 0, 1000);
+//			}
+//		}
+//		if(eff==ItemResources.STUN){
+//			target.isStunned=true;
+//			final Timer timer = new Timer();
+//			final TimerTask applyStun = new TimerTask(){
+//				public void run(){
+//					System.out.println("STUNNED");
+//					if(stunDuration>=3){
+//						stunDuration=0;
+//						target.isStunned=false;
+//						timer.cancel();
+//					}else{
+//						stunDuration++;
+//					}
+//				}
+//			};
+//			timer.scheduleAtFixedRate(applyStun, 1000, 1000);
+//		}
 	}
 
 }
