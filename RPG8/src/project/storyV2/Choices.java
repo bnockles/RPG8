f694936322f4c2280a0a227522e070babca6a6ca
mshahid5852 <mshahid5852@bths.edu>
2016-05-21 18:22:51 -0400

    Add files via upload

---
+++ RPG8/src/project/storyV2/Choices.java
@@ -0,0 +1,7 @@
+
+public interface Choices {
+	//This function will create a selectable menu box with two choices and those two choices will be displayed with strings
+	//option 1 and 2. When a user selects an option a boolean of true or false is returned. If option 1 is selected, true
+	//is returned and if option 2 is selected, then false is returned.
+	public boolean choiceMenu(String option1, String option2);
+}
