bad36bced1219be78f4847c7f16bcf9143d2c274
Maxwell <Maxwell@Max-PC.home>
2016-05-22 18:30:03 -0400

    MADE A MAN APPEAR

--- RPG8/src/project/tooltipdemo/DialogueDemo.java
+++ RPG8/src/project/tooltipdemo/DialogueDemo.java
@@ -4,10 +4,17 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import javax.imageio.ImageIO;
 
 import project.ChoiceMenuDemo.MenuTheme;
 import project.directors.Game;
 import project.directors.Screen;
+import project.menus.Tooltip;
 
 public class DialogueDemo extends Screen implements KeyListener {
 
@@ -44,12 +51,29 @@ public class DialogueDemo extends Screen implements KeyListener {
 		g2.setColor(MenuTheme.greenBR);
 		g2.fillRect(0, 0, width, height);
 		
+		//making memes appear
+		BufferedImage boxImg = null;
+		try {
+			boxImg = ImageIO.read(new File("resources/image1.jpg"));
+		} catch (IOException e) {
+		}
+		g2.drawImage(boxImg, 300, 130, 400, 400, null);
+		
 		//Text To Appear
 		g2.setColor(Color.BLACK);
 		g2.drawString("THIS IS A BAD TEST", 100, 100);
 		
+		//g2.drawString("THIS IS A GOOD TEST", 200, 100);
+		
 	}
 	
+	public void delayTime(){
+		try {
+		    TimeUnit.SECONDS.sleep(5);
+		} catch (InterruptedException e) {
+		    //Handle exception
+		}
+	}
 	
 
 }
