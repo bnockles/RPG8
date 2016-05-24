e436766a7e2ec008e67cc3794275519589fddde5
bnockles <bnockles@gmail.com>
2016-05-24 12:46:12 -0400

    fixed Ammo conflict

--- RPG8/src/project/items/Ammo.java
+++ RPG8/src/project/items/Ammo.java
@@ -9,12 +9,9 @@ public class Ammo extends Item{
 	private int ammoType;
 	private String itemImage;
 	private boolean buyable;
-<<<<<<< HEAD
 	private int burnDuration=0;
 	private int corrodeDuration;
 	private int stunDuration;
-=======
->>>>>>> refs/heads/develop
 	
 
 	public Ammo(String name, String desc,int cost,int ammoType, int effect, String itemImage, boolean buyable) {
@@ -41,56 +38,6 @@ public class Ammo extends Item{
 	public String getItemImage() {
 		return itemImage;
 	}
-<<<<<<< HEAD
-=======
-	
-	public void applyEffect(int eff,final TargetDemo target){
-		if(eff == ItemResources.FIRE){
-			if(!target.isBurned){
-				final Timer timer = new Timer();
-				final TimerTask countDown = new TimerTask(){
-					public void run(){
-						if(target.duration>=1){
-							target.duration=0;
-							target.isBurned=false;
-							timer.cancel();
-						}else{
-							target.health -=2;
-							if(target.health<0){
-								target.health=0;
-							}
-							target.duration++;
-						}
-					}
-				};
-				timer.scheduleAtFixedRate(countDown, 1000, 1000);
-				target.isBurned=true;
-			}	
-		}
-		if(eff == ItemResources.CORROSIVE){
-			
-		}
-		if(eff==ItemResources.STUN){
-			target.isStunned=true;
-			final Timer timer = new Timer();
-			final TimerTask countDown = new TimerTask(){
-				public void run(){
-					if(target.duration>=3){
-						target.duration=0;
-						target.isStunned=false;
-						timer.cancel();
-					}else{
-						target.duration++;
-					}
-				}
-			};
-			timer.scheduleAtFixedRate(countDown, 1000, 1000);
-		}
-	}
-	/** Type
-	 *[1-Regular, 2-Energy] Qing Ning Huang
-	 */
->>>>>>> refs/heads/develop
 	
 	public void applyEffect(int eff,final TargetDemo target){
 		if(eff != ItemResources.NO_EFFECT){
